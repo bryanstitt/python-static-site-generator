dd3c1d194e857faa362bdf02f8fa8d8d97dd6dfb
Bryan Stitt <bryan.stitt1@aa.com>
2021-11-17 09:50:07 -0600

    module 2 third attempt

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -11,24 +11,24 @@ class Parser:
         else:
             return False
 
-    def parse(path : Path, source : Path, dest : Path):
+    def parse(self, path : Path, source : Path, dest : Path):
         raise NotImplementedError
 
-    def read(path):
+    def read(self, path):
         with open(path) as file:
             return file.read()
     
-    def write(path, dest, content, ext=".html"):
+    def write(self, path, dest, content, ext=".html"):
         full_path = dest / path.with_suffix(ext).name
 
         with open(full_path) as file:
             file.write(content)
     
-    def copy(path, source, dest):
+    def copy(self, path, source, dest):
         shutil.copy2(path, dest / path.relative_to(source))
 
 class ResourceParser(Parser):
     extensions = [".jpg", ".png", ".gif", ".css", ".html"]
 
-    def parse(path: Path, source: Path, dest: Path):
+    def parse(self, path: Path, source: Path, dest: Path):
         Parser.copy(path, source, dest)
