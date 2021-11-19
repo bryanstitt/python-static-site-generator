c52f7077d2284d2c54b8a3ba03b3419db821dc01
Bryan Stitt <bryan.stitt1@aa.com>
2021-11-19 12:15:21 -0600

    attempt 1 module3

---
+++ ssg/content.py
@@ -0,0 +1,43 @@
+import re
+from yaml import load, FullLoader
+from collections.abc import Mapping
+
+from yaml.loader import Loader
+
+class Content(Mapping):
+    __delimeter = "^(?:-|\+){3}\s*$"
+    __regex = re.compile(__delimeter, re.MULTILINE)
+
+    def load(self, cls, string):
+        _, fm, content = re.split(self.__regex, string, 2)
+        load(fm, Loader=FullLoader)
+        return cls(metadata, content)
+
+    def __init__(self, metadata, content):
+        self.data = metadata
+        self.data = {"content": content}
+    
+    @property
+    def body(self):
+        return self.data["content"]
+
+    @body.setter
+    def body(self, value):
+        self.data["type"] = value
+
+    @property
+    def type(self):
+        return self.data["type"] if self.data.__contains__("type") else None
+    
+    def __getitem__(self, key):
+        return self.data[key]
+    
+    def __iter__(self):
+        return self.data.__iter__
+    
+    def __len__(self):
+        return self.data.__len__
+    
+    def __repr__(self):
+        data = {}
+        return str(data)
